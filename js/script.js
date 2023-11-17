74baf41f8bd4fa847f08444e4a5fbc290e3857a3
moonymel <musumeci.me@gmail.com>
2023-11-17 16:47:58 +0100

    ESERCIZIO COMPLETATO

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let name = prompt('Inserisci qui il tuo nome');
+console.log(name);
+
+let surname = prompt('Inserisci qui il tuo cognome');
+console.log(surname);
+
+let color = prompt('Inserisci qui il tuo colore preferito');
+console.log(color)
+
+document.getElementById('password').innerHTML = name + surname + color + '23'
